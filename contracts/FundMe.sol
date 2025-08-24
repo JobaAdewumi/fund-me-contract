ab32b0caf76ef8fca1b23166d8d0a6325db57771
Joba Adewumi <jobaadewumis@gmail.com>
2025-08-24 18:36:49 +0100

    Added complete contract with withdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,10 +9,16 @@ import './PriceConverter.sol';
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public minimumUsd = 10 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable{
 
@@ -21,7 +27,23 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    
+    function withdraw() public onlyOwner {
+        require(msg.sender == owner, "Sender is not owner");
+        for(uint256 fundersIndex = 0; fundersIndex < funders.length; fundersIndex++){
 
-    // function withdraw(){}
+            address funder = funders[fundersIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not owner");
+        _;
+    }
+
 }
